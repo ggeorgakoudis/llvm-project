dbd31cc952f0cc8989ef1e061a6fc678033cc9f3
gregboleet <gbolet@vt.edu>
2022-12-22 06:58:59 -0800

    backing up before proc-bind additions

--- runBuild.sh
+++ runBuild.sh
@@ -4,16 +4,17 @@
 #export CC=$(which clang)
 #export CXX=$(which clang++)
 
-module load gcc/10.2.1
+#module load gcc/10.2.1
 CC=$(which gcc)
 CXX=$(which g++)
 CXXFLAGS="-O3 -fopenmp"
 LDFLAGS=""
 
 module load ninja/1.9.0
+module load cmake
 
 cmake -G Ninja \
-        -DCMAKE_BUILD_TYPE='RelWithDebInfo' \
+        -DCMAKE_BUILD_TYPE='Release' \
         -DLLVM_ENABLE_PROJECTS='clang;openmp' \
         -DCMAKE_INSTALL_PREFIX=./install \
         -DLLVM_OPTIMIZED_TABLEGEN='On' \
@@ -21,5 +22,6 @@ cmake -G Ninja \
     	-DLLVM_USE_LINKER=gold \
         -DLLVM_ENABLE_ASSERTIONS='On' \
         ../llvm
+        #-DCMAKE_BUILD_TYPE='RelWithDebInfo' \
         #-DLLVM_CCACHE_BUILD='On' \
 	#-DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN='On'\
